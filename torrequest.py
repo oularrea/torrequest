5c8cb533242ecf77339d4d9da884e9a1a325add8
Osvaldo Ulises Larrea <oularrea@users.noreply.github.com>
2022-04-23 18:24:52 +0200

    Update torrequest.py
    add host

--- torrequest.py
+++ torrequest.py
@@ -9,10 +9,12 @@ import time
 
 class TorRequest(object):
   def __init__(self, 
+      host="localhost",
       proxy_port=9050, 
       ctrl_port=9051,
       password=None):
-
+    
+    self.host = host
     self.proxy_port = proxy_port
     self.ctrl_port = ctrl_port
     
@@ -25,8 +27,8 @@ class TorRequest(object):
 
     self.session = requests.Session()
     self.session.proxies.update({
-      'http': 'socks5://localhost:%d' % self.proxy_port,
-      'https': 'socks5://localhost:%d' % self.proxy_port,
+      'http': 'socks5://%s:%d' % (self.host, self.proxy_port),
+      'https': 'socks5://%s:%d' % (self.host, self.proxy_port),
     })
 
   def _tor_process_exists(self):
